252180d4532836ac75d15773a9f17f5a6473c4b5
MikeStef <micke.stefaniak@gmail.com>
2017-01-23 19:15:35 +0100

    Ändrade status bar color Gjorde så att appbar visas endast på add list activityn (mest ändringar i styles xml filen)

--- app/src/main/java/com/example/cba/shoppinglist/SecondActivity.java
+++ app/src/main/java/com/example/cba/shoppinglist/SecondActivity.java
@@ -9,5 +9,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        setTitle("Add list");
     }
 }
