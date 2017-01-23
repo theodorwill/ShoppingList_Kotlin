cc86d676a8c5889ae1d513ae8a47a028a14cbbba
MikeStef <micke.stefaniak@gmail.com>
2017-01-23 19:49:07 +0100

    Update layout/appbar Lade till plus ikonen för appbaren i add list activityn. Lade till två fält i layouten så att den inte är tom :)

--- app/src/main/java/com/example/cba/shoppinglist/SecondActivity.java
+++ app/src/main/java/com/example/cba/shoppinglist/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.cba.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         setTitle("Add list");
+    }
+
+    //Lägger till ikonerna i appbaren för denna activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
